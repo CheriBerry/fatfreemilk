bcd99db06081afe09a2e66ad6038e81a08c941a4
Berry <blaze12768@gmail.com>
2024-12-27 22:21:28 +0000

    changed things i guess

--- index.php
+++ index.php
@@ -2,29 +2,8 @@
 require("./vendor/autoload.php");
 $f3 = \F3::instance();
 $pd = \Parsedown::instance();
-$index = new class("temp.md"){
-    private string $name;
-    private $handle;
-    function __construct($name){
-        $this->name = $name;
-        $this->handle = fopen($name, "r");
-    }
-    function read(){
-        return file_get_contents($this->name);
-    }
-    function readM($size){
-        return fread($this->handle, $size);
-    }
-};
-echo("<link rel='stylesheet' href='style.css'>");
 $f3->route("GET /", function(){
-    global $f3, $pd, $index;
-    echo("<div>");
-    echo($pd->text($index->read()));
-    echo("</div>");
-});
-$f3->route("GET /file", function($f3){
-    echo(file_get_contents("./".$_GET["file"]));
+    global $f3, $pd;
 });
 $f3->run();
 ?>
