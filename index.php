30f7aa3baaed013a0d10804448d1769d7a277b89
Berry <blaze12768@gmail.com>
2024-12-28 02:35:20 +0000

    fixed random lines

--- index.php
+++ index.php
@@ -2,15 +2,10 @@
 require("./vendor/autoload.php");
 $f3 = \F3::instance();
 $pd = \Parsedown::instance();
-<<<<<<< HEAD
 $f3->route("GET /", function($f3){
 	global $pd;
 	echo($pd->text("# Welcome!"));
 	echo("this is working now :P");
-=======
-$f3->route("GET /", function(){
-    global $f3, $pd;
->>>>>>> bcd99db06081afe09a2e66ad6038e81a08c941a4
 });
 $f3->run();
 ?>
